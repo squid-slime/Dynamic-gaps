4950727e9dce381467cec7c16b0292450cd12bed
squid-slime <106412929+squid-slime@users.noreply.github.com>
2024-07-04 13:27:32 +0100

    Update dynamic_gaps.py
    added output customization

--- dynamic_gaps.py
+++ dynamic_gaps.py
@@ -14,16 +14,19 @@ if NOTIFY_AVAILABLE:
 # flag to control notifications
 enable_notifications = True        # CAPITALIZE first letter True False
 
+# Define the outputs to be affected
+AFFECTED_OUTPUTS = ["DP-1", "DP-2"]
+
 def set_gaps(connection, workspace, num_windows):
     try:
-        gap_size_hoz = None         #
-        gap_size_ver = None         # leave these at 0
-        gap_size_inner = None       #
+        gap_size_hoz = 0         #
+        gap_size_ver = 0         # leave these at 0
+        gap_size_inner = 0       #
 
         if num_windows == 1:
             gap_size_hoz = 500      # set horizontal gaps
             gap_size_ver = 75       # set vertical gaps
-            gap_size_inner = None      # set inner gaps
+            gap_size_inner = 0      # set inner gaps
         elif num_windows == 2:
             gap_size_hoz = 250
             gap_size_ver = 50
@@ -39,7 +42,7 @@ def set_gaps(connection, workspace, num_windows):
         elif num_windows == 5:
             gap_size_hoz = 40
             gap_size_ver = 45
-            gap_size_inner =  30
+            gap_size_inner = 30
         elif num_windows == 6:
             gap_size_hoz = 30
             gap_size_ver = 35
@@ -72,16 +75,21 @@ def set_gaps(connection, workspace, num_windows):
             notify2.Notification("Error", error_message, "dialog-error").show()
         else:
             print(error_message)
-            
+
+def get_output_of_workspace(workspace):
+    while workspace:
+        if workspace.type == 'output':
+            return workspace.name
+        workspace = workspace.parent
+    return None
+
 def update_gaps(connection, event=None):
     try:
-        current_workspace = connection.get_tree().find_focused().workspace().name
-        workspaces = connection.get_tree().workspaces()
-        for ws in workspaces:
-            if ws.name == current_workspace:
-                num_windows = len([node for node in ws.leaves() if not node.floating])
-                set_gaps(connection, current_workspace, num_windows)
-                break
+        current_workspace = connection.get_tree().find_focused().workspace()
+        current_output = get_output_of_workspace(current_workspace)
+        if current_output in AFFECTED_OUTPUTS:
+            num_windows = len([node for node in current_workspace.leaves() if not node.floating])
+            set_gaps(connection, current_workspace.name, num_windows)
     except Exception as e:
         error_message = f"An error occurred: {e}"
         if NOTIFY_AVAILABLE:
